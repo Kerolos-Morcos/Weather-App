cfe120e1e923285f1fec8211e74950479a472341
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-01-10 20:43:53 +0200

    Creating Cubit

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -4,14 +4,17 @@ import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
 
-class GetWeatherCubit extends Cubit<WeatherState>
-{
-  GetWeatherCubit(initialState) : super(initialState);
+class GetWeatherCubit extends Cubit<WeatherState> {
+  GetWeatherCubit() : super(InitialState());
 
-  getWeather({required String cityName}) async
-  {
-    WeatherModel weatherModel = await WeatherService(Dio()).getCurrentWeather(cityName: cityName);
-    emit(WeatherLoadedState());
-    weatherModel;
+  getWeather({required String cityName}) async {
+    try {
+      WeatherModel weatherModel =
+          await WeatherService(Dio()).getCurrentWeather(cityName: cityName);
+      emit(WeatherLoadedState());
+      return weatherModel;
+    } catch (e) {
+      emit(WeatherFailureState());
+    }
   }
 }
